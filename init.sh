29be7c16eb40f304fb40c3db3f7031799fa81c03
Luke Marney <marneylc@gmail.com>
2018-05-17 15:27:45 -0700

    nerdtree and ctrlp added

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# get some vim plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+
+# move vimrc to home directory
+cp ./.vimrc ~/.vimrc
