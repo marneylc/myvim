caf7b070e510fceccb87a828c23fc516c0737aee
Luke Marney <marneylc@gmail.com>
2019-11-27 17:17:25 -0800

    julia support

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+git clone git://github.com/JuliaEditorSupport/julia-vim.git ~/.vim/bundle/julia-vim
 
 # move vimrc to home directory
 cp -u .vimrc ~/.vimrc
