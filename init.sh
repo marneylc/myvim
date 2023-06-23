5451fd24994588c489c4527eda54730e214d27a9
Luke Marney <marneyl@oregonstate.edu>
2023-06-23 09:17:50 -0700

    remove solarized colors and added github copilot

--- init.sh
+++ init.sh
@@ -4,13 +4,10 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
-git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 git clone https://github.com/JuliaEditorSupport/julia-vim.git ~/.vim/bundle/julia-vim
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+git clone https://github.com/github/copilot.vim ~/.config/nvim/pack/github/start/copilot.vim
 
 # move vimrc to home directory
 cp -u .vimrc ~/.vimrc
-
-# move solarized colors to .vim/colors
-mkdir ~/.vim/colors/
-cp -u ~/.vim/bundle/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
+cp -r nvim ~/.config/
